9cac901a7af82e3801ef0db46210c15f0813027c
eliana <elianasouzarr@gmail.com>
2021-06-22 22:38:28 -0300

    API Pratica

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+module.exports = [
+   
+        {
+            method: 'GET',
+            path: '/products',
+            handler: function (request, h) {
+                return 'Hello Programador!';
+            }
+        }
+
+]
